1da5ecfdc11925ddfe9a86de924cf5a539a84680
Oliver Drobnik <oliver@cocoanetics.com>
2014-06-24 16:11:48 +0200

    Removed unncessary import, we have that in PCH anyway

--- Core/Source/DTOAuthWebViewController.h
+++ Core/Source/DTOAuthWebViewController.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Cocoanetics. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @class DTOAuthWebViewController;
 
 @protocol OAuthResultDelegate <NSObject>
--- Demo/Source/DiscogsViewController.h
+++ Demo/Source/DiscogsViewController.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Cocoanetics. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface DiscogsViewController : UIViewController
 
 - (IBAction)authorizeUser:(id)sender;
